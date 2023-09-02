aebbb377795969983af07d6d63bfa0638db743c5
George-Kibe <georgekibewambui@gmail.com>
2023-09-03 01:58:22 +0300

    standalone celery workers

---
+++ celeryworker/celeryconfig.py
@@ -0,0 +1,7 @@
+# import os
+
+# CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
+# CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
+
+broker_url = "redis://redis:6379/0"
+result_backend = "redis://redis:6379/0"
